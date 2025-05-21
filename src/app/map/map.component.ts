88682a759bd44f654d0510353b3a3a3e16b3e922
Peltimies <ac9194@student.jamk.fi>
2025-05-21 13:28:18 +0300

    feat: implement map component with geolocation tracking and notifications

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,21 +2,6 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 
-      // marker-ikoninen määrittelytiedostot
-      const iconRetinaUrl = 'assets/marker-icon-2x.png';
-      const iconUrl = 'assets/marker-icon.png';
-      const shadowUrl = 'assets/marker-shadow.png';
-      const iconDefault = L.icon({
-        iconRetinaUrl,
-        iconUrl,
-        shadowUrl,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        tooltipAnchor: [16, -28],
-        shadowSize: [41, 41],
-      });
-      
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -24,6 +9,8 @@ import * as L from 'leaflet';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
+
+
 // ! AfterViewInit suoritetaan kun näkymä on valmis.
 export class MapComponent implements AfterViewInit {
   private map: any;
@@ -76,6 +63,7 @@ export class MapComponent implements AfterViewInit {
       Notification.requestPermission().then((permission) => {
         // If the user accepts, let's create a notification
         if (permission === 'granted') {
+          console.log('Permission granted');
           const notification = new Notification('Terve!');
           // …
         }
