89202f57fce295547378bb5c5deef548d43881ed
Peltimies <ac9194@student.jamk.fi>
2025-05-21 13:00:00 +0300

    feat: add map component with geolocation and configure assets output path

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -20,19 +20,19 @@ export class MapComponent implements AfterViewInit {
       const latlng = new L.LatLng(location.coords.latitude, location.coords.longitude); 
       // luo uuden kartan ja asettaa sijainnin siihen, 13 tarkoittaa zoomia
 
-      const mymap = L.map('map').setView(latlng, 13);     
+      this.map = L.map('map').setView(latlng, 13);     
 
       // add the OpenStreetMap tiles
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      }).addTo(mymap);
+      }).addTo(this.map);
 
       // show the scale bar on the lower left corner
-      L.control.scale().addTo(mymap);
+      L.control.scale().addTo(this.map);
 
       // show a marker on the map
-      L.marker(latlng).bindPopup('The center of the world').addTo(mymap); 
+      L.marker(latlng).bindPopup('The center of the world').addTo(this.map); 
       
      });
   }
