033200671106e4694b3e602b44bd2ff514875cc0
Peltimies <ac9194@student.jamk.fi>
2025-05-21 12:54:06 +0300

    feat: add map component with geolocation and OpenStreetMap integration

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,16 +11,30 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: any;
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 3
-    });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    tiles.addTo(this.map);
+
+
+
+    // getCurrentPosition näyttää tämän hetkisen sijainnin
+	  navigator.geolocation.getCurrentPosition((location) => {
+      // latlng otetaan kordinaatit tämän hetkisen sijainnin
+      const latlng = new L.LatLng(location.coords.latitude, location.coords.longitude); 
+      // luo uuden kartan ja asettaa sijainnin siihen, 13 tarkoittaa zoomia
+
+      const mymap = L.map('map').setView(latlng, 13);     
+
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(mymap);
+
+      // show the scale bar on the lower left corner
+      L.control.scale().addTo(mymap);
+
+      // show a marker on the map
+      L.marker(latlng).bindPopup('The center of the world').addTo(mymap); 
+      
+     });
   }
   constructor() { }
 
